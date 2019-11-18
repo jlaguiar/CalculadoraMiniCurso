4eb39b5dd5659d0bb59fd459c04de6fcd55230cf
joao luiz takayama <takayama585@gmail.com>
2019-11-18 14:54:16 -0400

    Botoes e acoes

---
+++ app/src/main/java/com/jlaguiar/calculadoraminicurso/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jlaguiar.calculadoraminicurso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
